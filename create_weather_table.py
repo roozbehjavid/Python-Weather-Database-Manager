f4c6a4bc8efe61bfb43e0523a5ea930ba6422fee
Roozbeh <91006940+roozbehjavid@users.noreply.github.com>
2024-12-06 20:51:53 +0330

    Update create_weather_table.py

--- create_weather_table.py
+++ create_weather_table.py
@@ -2,14 +2,15 @@ import sqlite3 as sq
 
 with sq.connect(dbname) as conn:
     cursor = conn.cursor()
-    sql_create_table = 
-    f"CREATE TABLE IF NOT EXISTS {tablename} (
+    sql_create_table = f"""
+    CREATE TABLE IF NOT EXISTS {tablename} (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         city_name TEXT NOT NULL,
         temperature REAL,
         weather_condition TEXT,
         humidity INTEGER,
         wind REAL,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP );"
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    );"""
     cursor.execute(sql_create_table)
     conn.commit()
